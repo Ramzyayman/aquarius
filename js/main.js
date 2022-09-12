3160e2a2b947f1308660925818e6980d4f09b052
Ramzyayman <36305487+Ramzyayman@users.noreply.github.com>
2022-09-12 06:54:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var fb = document.getElementById("fb")
+var ig = document.getElementsByClassName("ig")[0]
+var twitter = document.getElementsByClassName("twitter")[0]
+
+fb.onclick = function () {
+
+    window.open("https://www.facebook.com/rxmzzii", "_blank")
+}
+
+
+ig.onclick = function () {
+
+    window.open("https://www.instagram.com/rxmzzii", "_blank")
+}
+
+twitter.onclick = function () {
+
+    window.open("https://www.twitter.com/rxmzzii", "_blank")
+}
